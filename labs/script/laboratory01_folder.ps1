989c72a5114cbce65fd858878609278fb5975075
Itamar de Sá Britto Júnior <80598950+itamarsb@users.noreply.github.com>
2026-07-24 16:53:37 -0300

    Create laboratory01_folder.ps1

---
+++ labs/script/laboratory01_folder.ps1
@@ -0,0 +1,13 @@
+mkdir labs\01-weather-api
+mkdir labs\01-weather-api\app
+mkdir labs\01-weather-api\docs
+mkdir labs\01-weather-api\docs\images
+
+New-Item labs\01-weather-api\app\__init__.py -ItemType File
+New-Item labs\01-weather-api\app\data.py -ItemType File
+New-Item labs\01-weather-api\app\main.py -ItemType File
+New-Item labs\01-weather-api\requirements.txt -ItemType File
+New-Item labs\01-weather-api\.gitignore -ItemType File
+New-Item labs\01-weather-api\README.md -ItemType File
+
+cd labs\01-weather-api
