ce94c71413ba55bd55dae7639dc15cb22375df9f
Itamar de Sá Britto Júnior <80598950+itamarsb@users.noreply.github.com>
2026-07-23 22:47:38 -0300

    Update example_postman.js

--- labs/script/example_postman.js
+++ labs/script/example_postman.js
@@ -1,11 +1,11 @@
 // EN: Checks if the API responded successfully (HTTP 200)
-// Verifica se a API respondeu com sucesso (HTTP 200)
+// PT-BR: Verifica se a API respondeu com sucesso (HTTP 200)
 pm.test("Status code is 200", function () {
     pm.response.to.have.status(200);
 });
 
 // EN: Ensures the response body contains the ID of the created/requested record
-// Garante que o corpo da resposta traz o ID do registro criado/solicitado
+// PT-BR: Garante que o corpo da resposta traz o ID do registro criado/solicitado
 pm.test("Response contains task ID", function () {
     const response = pm.response.json();
     pm.expect(response).to.have.property("id");
